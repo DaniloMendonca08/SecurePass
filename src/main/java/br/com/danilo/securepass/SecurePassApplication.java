45fa7a189f43fb5be8eceb451fc7a0315e9eddc0
Danilo Mendonça <danilomendonca08@gmail.com>
2025-03-18 21:30:01 -0300

    chore: add project files

---
+++ src/main/java/br/com/danilo/securepass/SecurePassApplication.java
@@ -0,0 +1,13 @@
+package br.com.danilo.securepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurePassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurePassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/danilo/securepass/SecurePassApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danilo.securepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
