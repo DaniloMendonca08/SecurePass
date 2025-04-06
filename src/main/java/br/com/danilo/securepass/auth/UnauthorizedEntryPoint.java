618edfc5fd435d838bb7c1ca21eb0d13ec1ec809
Danilo Mendonça <danilomendonca08@gmail.com>
2025-04-05 22:22:58 -0300

    feat: add UnauthorizedEntryPoint for exceptionHandling

---
+++ src/main/java/br/com/danilo/securepass/auth/UnauthorizedEntryPoint.java
@@ -0,0 +1,27 @@
+package br.com.danilo.securepass.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class UnauthorizedEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("""
+            {
+                "message": "Para utilizar este endpoint é necessário estar autenticado.",
+                "code": "AUTH_REQUIRED"
+            }
+        """);
+    }
+}
