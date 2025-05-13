7d66ee5c36b937b50cd2f18fca0bb51b5b0ba684
Danilo Mendonça <danilomendonca08@gmail.com>
2025-05-12 21:47:51 -0300

    docs: add Swagger annotations to AuthController and UserController

--- src/main/java/br/com/danilo/securepass/auth/AuthController.java
+++ src/main/java/br/com/danilo/securepass/auth/AuthController.java
@@ -1,6 +1,8 @@
 package br.com.danilo.securepass.auth;
 
 import br.com.danilo.securepass.user.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Autenticação do usuário", description = "Endpoint responsável por realizar a autenticação de usuários.")
 @Slf4j
 @RestController
 public class AuthController {
@@ -23,6 +26,10 @@ public class AuthController {
         this.tokenService = tokenService;
     }
 
+    @Operation(
+        summary = "Realiza login do usuário",
+        description = "Realiza login do usuário e retorna um token de acesso em caso de sucesso."
+    )
     @PostMapping("/login")
     public Token login(@RequestBody Credentials credentials) {
         log.debug("Realizando login do usuário...");
--- src/main/java/br/com/danilo/securepass/user/UserController.java
+++ src/main/java/br/com/danilo/securepass/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.danilo.securepass.user;
 
 import br.com.danilo.securepass.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -9,6 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@Tag(name = "Gerenciamento de usuários", description = "Endpoints para criação, atualização, exclusão e consulta de informações dos usuários.")
 @Slf4j
 @RestController
 @RequestMapping("/user")
@@ -20,6 +23,11 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation
+        (
+            summary = "Cria um novo usuário",
+            description = "Recebe os dados do usuário no corpo da requisição, cria o usuário e retorna o recurso criado."
+        )
     @PostMapping
     public ResponseEntity<User> create(@Valid @RequestBody User user, UriComponentsBuilder uriBuilder) {
         log.info("Requisição recebida para criar um novo usuário.");
@@ -37,6 +45,11 @@ public class UserController {
 
     }
 
+    @Operation
+        (
+            summary = "Exclui um usuário existente",
+            description = "Realiza a exclusão de um usuário com base no ID fornecido. Retorna uma resposta indicando se teve sucesso ou falha na execução."
+        )
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse> delete(@PathVariable Long id) {
         log.info("Requisição recebida para deletar um usuário baseado em seu ID.");
@@ -48,6 +61,10 @@ public class UserController {
                 .body(new ApiResponse("Usuário não foi encontrado para exclusão."));
     }
 
+    @Operation(
+        summary = "Busca informações do usuário logado",
+        description = "Retorna os dados do usuário autenticado."
+    )
     @GetMapping("/info")
     public ResponseEntity<User> getInfo(@AuthenticationPrincipal String username) {
         log.debug("Requisição recebida para buscar as informações do usuário logado.");
@@ -57,6 +74,10 @@ public class UserController {
         return ResponseEntity.ok(user);
     }
 
+    @Operation(
+        summary = "Atualiza dados do usuário",
+        description = "Com os dados recebidos no corpo da requisição, atualiza o usuário no sistema e retorna suas informações atualizadas."
+    )
     @PutMapping("/update")
     public ResponseEntity<User> update(@Valid @RequestBody UpdateUserDTO updateUserDTO, @AuthenticationPrincipal String username) {
         log.debug("Requisição recebida para atualizar os dados do usuário.");
